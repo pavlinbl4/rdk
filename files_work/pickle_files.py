5237b81592cd37e641e501c3a037b2b71b33d6be
evgeniy_MacBook <pavlinbl4@gmail.com>
2025-01-05 21:33:56 +0300

    refactoring

--- files_work/pickle_files.py
+++ files_work/pickle_files.py
@@ -1,6 +1,8 @@
-import pickle
 import os
+import pickle
 import re
+from typing import BinaryIO
+
 from loguru import logger
 
 
@@ -10,40 +12,30 @@ def file_name(pickle_file):
 
 def load_pickle(path_to_pickle_folder, pickle_file_name):
     try:
-        with open(f'{path_to_pickle_folder}/{pickle_file_name}.pickle', 'rb') as articles:
+        file_path = os.path.join(path_to_pickle_folder, f'{pickle_file_name}.pickle')
+        with open(file_path, 'rb') as articles:
             article_dict = pickle.load(articles)
         return article_dict
     except FileNotFoundError as file_error:
-        logger.error(file_error)
+        logger.error(f"File not found: {file_error}")
+        return None
 
 
-def dump_pickle(path_to_pickle_folder, pickle_file_name, article_dict=None):
-    # logger.info(f'{path_to_pickle_folder = }')
+def dump_pickle(path_to_pickle_folder: str, pickle_file_name: str, article_dict = None) -> None:
+    logger.info(f'{path_to_pickle_folder = }')
     if article_dict is None:
         article_dict = {}
-    with open(f'{path_to_pickle_folder}/{pickle_file_name}.pickle', 'wb') as f:
+    file_path = os.path.join(path_to_pickle_folder, f'{pickle_file_name}.pickle')
+    f: BinaryIO
+    with open(file_path, 'wb') as f:  # f is of type BinaryIO
         pickle.dump(article_dict, f)
-        # logger.info(f"file {pickle_file_name} updated")
+        logger.info(f"File {pickle_file_name} updated")
 
 
-def delete_old_pickle(today_filename: str, path_to_pickle_folder: str):
+def delete_old_pickle(today_filename: str, path_to_pickle_folder: str) -> None:
     for file in os.listdir(path_to_pickle_folder):
         if file.endswith('pickle'):
             pickle_file = os.path.join(path_to_pickle_folder, file)
             if file != f'{today_filename}.pickle':
                 os.remove(pickle_file)
-                logger.info(f"file {file} removed")
-
-
-if __name__ == '__main__':
-    # ic(load_pickle('/Volumes/big4photo/_PYTHON/@_EDICATION/OOP/rdk_cheker/Pickle_files', '28-02-2024'))
-    e_dict = {'СКА СТАВИМ СРАЗУ': 'W&site',
-              'невкосметика ставить немедля': 'W&site',
-              'парнас ставить сразу ж': 'W&site',
-              'рынок рекламы на утро': 'E&site',
-              'театрал срочно в номер': 'W&site',
-              'шевченко ставить тотчас': 'W&site'}
-    dump_pickle('Pickle_files', '27-02-2024')
-
-    # delete_old_pickle(path_to_pickle_folder='/Volumes/big4photo/_PYTHON/@_EDICATION/OOP/rdk_cheker/Pickle_files',
-    #                   today_filename='28-02-2024')
+                logger.info(f"File {file} removed")
