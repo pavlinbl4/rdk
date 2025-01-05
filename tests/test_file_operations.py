f7d0c8eeedd7b4e3975bf988419293771c36c57e
evgeniy_MacBook <pavlinbl4@gmail.com>
2025-01-05 19:29:10 +0300

    test file

---
+++ tests/test_file_operations.py
@@ -0,0 +1,21 @@
+import os
+
+from files_work.check_existing_file import check_file, create_dir
+
+def test_check_file_existing():
+    # Относительный путь
+    assert check_file('/Users/evgeniy/_Python/rdk/requirements.txt') is True
+
+def test_check_file_non_existing():
+    assert check_file('non_existing_file.txt') is False
+
+def test_create_dir():
+    # Создаем директорию в корне
+    folder_name = 'TestFolder'
+    folder_path = create_dir(folder_name)
+
+    assert os.path.exists(folder_path)
+    assert os.path.isdir(folder_path)
+
+    # Удаляем созданную директорию после теста
+    os.rmdir(folder_path)
