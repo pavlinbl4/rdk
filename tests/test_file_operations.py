dd90b6902f2b4c06d79e665e5f91811c69b44df9
evgeniy_MacBook <pavlinbl4@gmail.com>
2025-01-05 19:40:24 +0300

    решен вопрос с относительным путем к файлу ../requirements.txt

--- tests/test_file_operations.py
+++ tests/test_file_operations.py
@@ -3,14 +3,14 @@ import os
 from files_work.check_existing_file import check_file, create_dir
 
 def test_check_file_existing():
-    # Относительный путь
-    assert check_file('/Users/evgeniy/_Python/rdk/requirements.txt') is True
+    assert check_file('../requirements.txt') is True
+
 
 def test_check_file_non_existing():
     assert check_file('non_existing_file.txt') is False
 
 def test_create_dir():
-    # Создаем директорию в корне
+    # Создаем директорию в корне скрипта
     folder_name = 'TestFolder'
     folder_path = create_dir(folder_name)
 
