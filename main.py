31bec52e275cb6fc91fc89c189bb354b06e4bf40
bigMac <bigMac@bigMac>
2025-02-17 22:18:07 +0300

    added encoding="utf-8" to logger

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
+import os
+
+from loguru import logger
+
 from browser.driver_job import get_work_map
 from check_info.check_article_status import check_article_status
 from files_work.check_existing_file import create_dir
-from files_work.delete_old_files import delete_old_log_files
 from files_work.pickle_files import delete_old_pickle, dump_pickle, load_pickle
 from time_tools.time_zone import get_city_time
-from loguru import logger
-import os
 
 logs_dir = create_dir('Logs')
-logger.add(f'{logs_dir}/debug.log', rotation="10:00", retention="3 days", compression="zip")
+logger.add(f'{logs_dir}/debug.log', rotation="10:00", retention="3 days", compression="zip", encoding="utf-8")
 
 
 def get_article_status():
